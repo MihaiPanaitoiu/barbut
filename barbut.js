5e482ca200ca4b9ae2e43182becc5040ccd56f8d
Mihai Panaitoiu <varusovarus1328@gmail.com>
2018-11-15 00:09:01 +0200

    Make buttons throw dice

--- barbut.js
+++ barbut.js
@@ -1,3 +1,24 @@
+var diceDsp11 = document.querySelector("#span1");
+var diceDsp12 = document.querySelector("#span2");
+var diceDsp21 = document.querySelector("#span3");
+var diceDsp22 = document.querySelector("#span4");
+var buton1 = document.querySelector("#button1");
+var buton2 = document.querySelector("#button2");
+
+
+buton1.addEventListener("click", function(){
+	diceDsp11.textContent = zar();
+	diceDsp12.textContent = zar();
+});
+
+buton2.addEventListener("click", function(){
+	diceDsp21.textContent = zar();
+	diceDsp22.textContent = zar();
+});
+
+
+
 function zar(){
 	return Math.floor(Math.random() * 6 + 1);
 }
+
