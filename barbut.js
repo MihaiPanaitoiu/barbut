9f4a4fc08c9eb1560ff1478f6763ffaf47b45ea1
Mihai Panaitoiu <varusovarus1328@gmail.com>
2018-11-16 17:44:20 +0200

    Indent the code

--- barbut.js
+++ barbut.js
@@ -4,16 +4,23 @@ var diceDsp21 = document.querySelector("#span3");
 var diceDsp22 = document.querySelector("#span4");
 var buton1 = document.querySelector("#button1");
 var buton2 = document.querySelector("#button2");
+var roundOver = 2;
+var roundOverCheck = 0
+
 
 
 buton1.addEventListener("click", function(){
 	diceDsp11.textContent = zar();
 	diceDsp12.textContent = zar();
+	roundOverCheck+=1;
+	console.log(roundOverCheck);
 });
 
 buton2.addEventListener("click", function(){
 	diceDsp21.textContent = zar();
 	diceDsp22.textContent = zar();
+	roundOverCheck+=1;
+	console.log(roundOverCheck);
 });
 
 
