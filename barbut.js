dcace77853e2f852430fb5b50bd6319cc87ff879
MihaiPanaitoiu <varusovarus1328@gmail.com>
2018-11-16 13:40:46 +0200

    Add the score logic

--- barbut.js
+++ barbut.js
@@ -12,23 +12,90 @@ var zar2 = zar();
 var zar3 = zar();
 var zar4 = zar();
 
-buton1.addEventListener("click", function(){
-	diceDsp11.textContent = zar1;
-	diceDsp12.textContent = zar2;
-	p1Score += zar1 + zar2;
-	return p1Score;
-});
 
-buton2.addEventListener("click", function(){
-	diceDsp21.textContent = zar3;
-	diceDsp22.textContent = zar4;
-	p2Score += zar3 + zar4;
-	return p2Score;
-});
-
+function winnerCheck(){
+	if (p1Score > p2Score) {
+		diceDsp11.classList.add("winner");
+		console.log("player 1 winner");
+	}
+}
 
 
 function zar(){
 	return Number(Math.floor(Math.random() * 6 + 1));
 }
+
+
+function round() {
+	buton1.addEventListener("click", function() {
+	if (!roundOver) {
+		p1Score += zar1 + zar2;
+	}
+	diceDsp11.textContent = zar1;
+	diceDsp12.textContent = zar2;
+	})
+
+	buton2.addEventListener("click", function() {
+		if (!roundOver) {
+			p2Score += zar3 + zar4;
+		}
+		diceDsp21.textContent = zar3;
+		diceDsp22.textContent = zar4;
+	})
+
+	if (p1Score > p2Score) {
+		roundOver = true;
+		console.log("a castigat p1");
+	}
+
+	else if(p2Score > p1Score) {
+		roundOver = true;
+		console.log("a castigat p2");
+	}
+}
+
+function init() {
+	round();
+}
+
+init();
+
+// buton1.addEventListener("click", function() {
+// 	if (!roundOver) {
+// 		p1Score += zar1 + zar2;
+// 		p2Score += zar3 + zar4;
+// 	}
+// 	if (p1Score > p2Score) {
+// 		roundOver = true;
+// 		console.log("a castigat p1");
+// 	}
+// 	diceDsp11.textContent = zar1;
+// 	diceDsp12.textContent = zar2;
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
