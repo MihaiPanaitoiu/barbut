933b7e934ad5ee997a0a4c6b675332f37d57ac9b
Mihai Gătejescu <gus666xe@gmail.com>
2018-11-19 19:08:01 +0200

    Disable buttons after first press

--- barbut.js
+++ barbut.js
@@ -5,7 +5,6 @@ var diceDsp22 = document.querySelector("#span4");
 var buton1 = document.querySelector("#button1");
 var buton2 = document.querySelector("#button2");
 
-var roundOver = false;
 var p1Score = 0;
 var p2Score = 0;
 var zar1 = zar();
@@ -14,48 +13,70 @@ var zar3 = zar();
 var zar4 = zar();
 
 
-function winnerCheck(){
-	if (p1Score > p2Score) {
-		diceDsp11.classList.add("winner");
-		console.log("player 1 winner");
-	}
-}
+// function winnerCheck(){
+// 	if (p1Score > p2Score) {
+// 		diceDsp11.classList.add("winner");
+// 		console.log("player 1 winner");
+// 	}
+// }
 
 function zar(){
 	return Number(Math.floor(Math.random() * 6 + 1));
 }
 
+function announceWinner()
+{
+    console.log("Winner");
+}
+
+function resetRound()
+{
+    console.log("Round over");
+    roundOver = 0;
+}
+
 
 function round() {
-	buton1.addEventListener("click", function() {
-	if (!roundOver) {
-		p1Score += zar1 + zar2;
-	}
-	diceDsp11.textContent = zar1;
-	diceDsp12.textContent = zar2;
-	})
 
-	buton2.addEventListener("click", function() {
-		if (!roundOver) {
-			p2Score += zar3 + zar4;
-		}
-		diceDsp21.textContent = zar3;
-		diceDsp22.textContent = zar4;
-	})
+    var roundOver = 0;
+    
+    buton1.addEventListener("click", function() {
+        if (roundOver < 2) {
+            p1Score += zar1 + zar2;
+            buton1.disabled = true;
+        }
+        diceDsp11.textContent = zar1;
+        diceDsp12.textContent = zar2;
+        roundOver++;
+        if (roundOver >= 2)
+            announceWinner();
+    })
 
-	if (p1Score > p2Score) {
-		roundOver = true;
-		console.log("a castigat p1");
-	}
+    buton2.addEventListener("click", function() {
+        if (roundOver < 2) {
+            p2Score += zar3 + zar4;
+            buton2.disabled = true;
+        }
+        diceDsp21.textContent = zar3;
+        diceDsp22.textContent = zar4;
+        roundOver++;
+        if (roundOver >= 2)
+            announceWinner();
+    })
 
-	else if(p2Score > p1Score) {
-		roundOver = true;
-		console.log("a castigat p2");
-	}
+	// if (p1Score > p2Score) {
+	// 	roundOver = true;
+	// 	console.log("a castigat p1");
+	// }
+
+	// else if(p2Score > p1Score) {
+	// 	roundOver = true;
+	// 	console.log("a castigat p2");
+	// }
 }
 
 function init() {
-	round();
+    round();
 }
 
 init();
